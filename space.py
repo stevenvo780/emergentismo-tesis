0c470b4ed4ce83b09d12b113e007b4ea5964f7de
Stev <stevenvallejo780@gmail.com>
2023-08-21 13:32:18 -0500

    feat

--- space.py
+++ space.py
@@ -2,28 +2,34 @@ from types_universo import NodoInterface, systemRules, IPhysicsRules, Relacion
 from time_procedural import calcular_energia_matricial, intercambiar_cargas_matricial, calcular_distancias_matricial, relacionar_nodos_matricial, calcular_cargas, liberar_memoria_gpu
 from random import uniform
 from typing import List
-import cupy as cp
 from concurrent.futures import ThreadPoolExecutor
-import time
-
+from typing import List
+import numpy as np
 
 def next_step(nodos: List[NodoInterface], valores_sistema: IPhysicsRules):
+    print("Calculando distancias matriciales...")
     matriz_distancias = calcular_distancias_matricial(nodos)
+    print("Calculando relaciones matriciales...")
     matriz_relaciones = relacionar_nodos_matricial(
         valores_sistema, nodos, matriz_distancias)
     del matriz_distancias
+    print("Calculando nuevas cargas...")
     cargas_nuevas = calcular_cargas(nodos, valores_sistema)
+    print("Calculando energías matriciales...")
     energias = calcular_energia_matricial(nodos, matriz_relaciones)
+    print("Intercambiando cargas matriciales...")
     matriz_cargas = intercambiar_cargas_matricial(
         valores_sistema, nodos, matriz_relaciones)
-    cargas_nuevas += cp.sum(matriz_cargas, axis=1)
+    cargas_nuevas += np.sum(matriz_cargas, axis=1)
     del matriz_cargas
     liberar_memoria_gpu()
     for i, nodo in enumerate(nodos):
         nodo.cargas = cargas_nuevas[i].tolist()
         nodo.energia = energias[i].tolist()
         nodo.relaciones_matriz = matriz_relaciones[i]
-    time.sleep(1)
+    del matriz_relaciones
+    del cargas_nuevas
+    print("Proceso completado.")
     return nodos
 
 def crear_nodo(i: int, j: int, cargas: float, energia: float) -> NodoInterface:
@@ -33,7 +39,6 @@ def crear_nodo(i: int, j: int, cargas: float, energia: float) -> NodoInterface:
         energia=energia,
         relaciones_matriz=[],
     )
-
 
 def expandir_espacio(nodos: List[NodoInterface]) -> List[NodoInterface]:
     for i in range(systemRules.CRECIMIENTO_X):
--- time_procedural.py
+++ time_procedural.py
@@ -3,40 +3,24 @@ from types_universo import NodoInterface, IPhysicsRules, systemRules
 import numpy as np
 import cupy as cp
 
-
 def intercambiar_cargas_matricial(valores_sistema: IPhysicsRules, nodos: List[NodoInterface], matriz_relaciones: cp.ndarray) -> cp.ndarray:
-    matriz_cargas = matriz_relaciones  # Ya tenemos la matriz de relaciones calculada
     cargas = cp.array([nodo.cargas for nodo in nodos], dtype=cp.float32)
-    matriz_cargas += (cargas[:, None] + cargas) / 2
+    matriz_cargas = matriz_relaciones + cargas[:, None]
     liberar_memoria_gpu()
     return matriz_cargas
 
-
 def calcular_energia_matricial(nodos: List[NodoInterface], matriz_relaciones: cp.ndarray) -> cp.ndarray:
-    # Utilizamos la matriz de relaciones ya calculada
     matriz_adyacencia = matriz_relaciones
-
-    # Calcular la carga en cadena para cada nodo
-    carga_en_cadena = cp.sum(matriz_adyacencia, axis=1)
-
-    # Calcular la energía para cada nodo
-    energias = 1 - cp.abs(cp.array([nodo.cargas for nodo in nodos])) + cp.abs(carga_en_cadena)
+    carga_en_cadena_abs = cp.abs(cp.sum(matriz_adyacencia, axis=1))
+    energias = 1 - cp.abs(cp.array([nodo.cargas for nodo in nodos])) + carga_en_cadena_abs
     energias = cp.clip(energias, None, 1)
     liberar_memoria_gpu()
     return energias
 
-
-def liberar_memoria_gpu():
-    cp.cuda.Stream.null.synchronize()
-    cp.cuda.MemoryPool().free_all_blocks()
-
-
 def calcular_distancias_matricial(nodos: List[NodoInterface]) -> cp.ndarray:
-    coords = cp.array([list(map(int, nodo.id.split('-')[1:]))
-                      for nodo in nodos], dtype=cp.float32)
-    i_diff = coords[:, 0][:, None] - coords[:, 0]
-    j_diff = coords[:, 1][:, None] - coords[:, 1]
-    matriz_distancias = cp.hypot(i_diff, j_diff)
+    coords = cp.array([list(map(int, nodo.id.split('-')[1:])) for nodo in nodos], dtype=cp.float32)
+    diff = coords[:, None, :] - coords
+    matriz_distancias = cp.linalg.norm(diff, axis=-1)
     liberar_memoria_gpu()
     return matriz_distancias
 
@@ -51,15 +35,13 @@ def relacionar_nodos_matricial(valores_sistema: IPhysicsRules, nodos: List[NodoI
     non_zero_mask = matriz_distancias != 0
 
     probabilidad_relacion = cp.where(
-        non_zero_mask & (matriz_distancias <
-                         systemRules.DISTANCIA_MAXIMA_RELACION),
-        diferencia_cargas / 2 / matriz_distancias,
+        non_zero_mask & (matriz_distancias < systemRules.DISTANCIA_MAXIMA_RELACION),
+        diferencia_cargas / (2 * matriz_distancias),
         0
     ) * valores_sistema.FACTOR_RELACION
 
-    relacion_mask = (cp.random.rand(*probabilidad_relacion.shape)
-                     < probabilidad_relacion) & cargas_mask
-    relacion_mask &= energias_mask[:, None]
+    relacion_mask = cp.random.rand(*probabilidad_relacion.shape) < probabilidad_relacion
+    relacion_mask &= cargas_mask & energias_mask[:, None]
 
     carga_compartida = (cargas[:, None] + cargas) / 2
     matriz_relaciones = cp.where(relacion_mask, carga_compartida, 0)
@@ -68,29 +50,25 @@ def relacionar_nodos_matricial(valores_sistema: IPhysicsRules, nodos: List[NodoI
 
 
 def calcular_cargas(nodos: List[NodoInterface], valores_sistema: IPhysicsRules) -> cp.ndarray:
-    # Crear un array de cargas en la GPU utilizando CuPy
     cargas = cp.array([float(nodo.cargas) for nodo in nodos], dtype=cp.float32)
 
-    # Aplicar la probabilidad de transición
-    transicion_indices = cp.random.rand(
-        len(nodos)) < valores_sistema.PROBABILIDAD_TRANSICION
+    transicion_indices = cp.random.rand(len(nodos)) < valores_sistema.PROBABILIDAD_TRANSICION
     cargas[transicion_indices] *= -1
 
-    # Aplicar fluctuación
     fluctuacion = cp.random.uniform(-valores_sistema.FLUCTUACION_MAXIMA,
                                     valores_sistema.FLUCTUACION_MAXIMA, len(nodos))
     cargas += fluctuacion
 
-    # Aplicar fluctuación aleatoria
-    aleatorio_indices = cp.random.rand(len(nodos)) < 0.5
-    cargas += fluctuacion * (1 - 2 * aleatorio_indices)
+    aleatorio_factor = cp.random.choice([1, -1], len(nodos))
+    cargas += fluctuacion * aleatorio_factor
 
-    # Aplicar probabilidad de túnel
-    tunel_indices = (cargas > 0.5) & (cp.random.rand(
-        len(nodos)) < valores_sistema.PROBABILIDAD_TUNEL)
+    tunel_indices = (cargas > 0.5) & (cp.random.rand(len(nodos)) < valores_sistema.PROBABILIDAD_TUNEL)
     cargas[tunel_indices] = 0
 
-    # Limitar las cargas a [-1, 1]
     cargas = cp.clip(cargas, -1, 1)
     liberar_memoria_gpu()
     return cargas
+
+def liberar_memoria_gpu():
+    cp.cuda.Stream.null.synchronize()
+    cp.cuda.MemoryPool().free_all_blocks()
